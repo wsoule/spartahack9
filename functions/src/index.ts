633d3f2193cdf5f8b4f4f49d3150b197a81f8649
KadinMEastway <Kadin.eastway2021@gmail.com>
2024-01-27 13:13:35 -0500

    Firebase correction + Firestore created + functions init

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,7 +13,7 @@ const bucketName = 'wastewell-8a42f';
 export const imageTagger = functions.storage
   .bucket(bucketName)
   .object()
-  .onMetadataUpdate( async event => {
+  .onMetadataUpdate(async event => {
     // File data
     const object = event.metadata;
     const filePath = object.name;
@@ -22,7 +22,7 @@ export const imageTagger = functions.storage
     const imageUri = `gs://${bucketName}/vision/${filePath}`;
 
     // Firestore docId === file name
-    const docId =  filePath.split('.jpg')[0];
+    const docId = filePath.split('.jpg')[0];
 
     const docRef = admin.firestore().collection('photos').doc(docId);
 
@@ -33,6 +33,6 @@ export const imageTagger = functions.storage
     const labels = results[0].labelAnnotations?.map(obj => obj.description);
     const metal = labels?.includes('metal');
 
-    return docRef.set({metal, labels});
+    return docRef.set({ metal, labels });
   }
   )
