a1136a0af0fc083e23c8b2c1064795d51fdf363b
KadinMEastway <Kadin.eastway2021@gmail.com>
2024-01-27 16:56:21 -0500

    unbreaking...

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import {
   Alert,
@@ -8,11 +9,16 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
+=======
+import React from 'react';
+import { Alert, Button, Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+>>>>>>> 0c80e50 (unbreaking...)
 
 const WelcomePage: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to WasteWell!</Text>
+<<<<<<< HEAD
       <Text style={styles.subtitle}>
         WasteWell's mission is to make recycling easy and fun. We aim to educate
         and engage users in sustainable practices through a simple, interactive
@@ -29,14 +35,30 @@ const WelcomePage: React.FC = () => {
         style={styles.signUpButton}
         onPress={() => {
           Alert.alert("You tapped the sign up button!");
+=======
+      <Text style={styles.subtitle}>WasteWell's mission is to make recycling easy and fun. We aim to educate and engage users in sustainable practices through a simple, interactive app that identifies recyclable items with just a photo. Join us in our journey to inspire a greener lifestyle, one piece of trash at a time.</Text>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('@/assets/images/recycle.png')}/>
+      </View>
+      <TouchableOpacity
+      style={styles.signUpButton}
+        onPress={() => {
+          Alert.alert('You tapped the sign up button!');
+>>>>>>> 0c80e50 (unbreaking...)
         }}
       >
         <Text style={styles.buttonText}>Join Now</Text>
       </TouchableOpacity>
       <TouchableOpacity
+<<<<<<< HEAD
         style={styles.loginButton}
         onPress={() => {
           Alert.alert("You tapped the SIGN IN button!");
+=======
+      style={styles.loginButton}
+        onPress={() => {
+          Alert.alert('You tapped the SIGN IN button!');
+>>>>>>> 0c80e50 (unbreaking...)
         }}
       >
         <Text style={styles.buttonText}>Sign In</Text>
@@ -47,6 +69,7 @@ const WelcomePage: React.FC = () => {
 
 const styles = StyleSheet.create({
   container: {
+<<<<<<< HEAD
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
@@ -58,10 +81,25 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 10,
     color: "#388E3C", // Darker text for better readability
+=======
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#F5F5F5', // A light grey background
+    height: '100%',
+    
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#388E3C', // Darker text for better readability
+>>>>>>> 0c80e50 (unbreaking...)
   },
   subtitle: {
     fontSize: 18,
     marginBottom: 20,
+<<<<<<< HEAD
     textAlign: "center",
     color: "#555", // Slightly lighter text for the subtitle
   },
@@ -103,6 +141,51 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
+=======
+    textAlign: 'center',
+    color: '#555', // Slightly lighter text for the subtitle
+  },
+  imageContainer: {
+    backgroundColor: '#607D8B', // Replace with your dark color
+    borderRadius: 100, // Adjust for blob-like shape
+    padding: 10, // Adjust as needed
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '30%', // Change as per your requirement
+    width: '70%'
+  },
+  image: {
+    width: '100%', // Adjust as per your requirement
+    height: '100%', // Adjust as per your requirement
+    resizeMode: 'contain', // or 'contain' based on your preference
+  },
+  loginButton: {
+    marginTop: 30,
+    backgroundColor: '#4682b4',
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+    width: '80%',
+    height: 50,
+    justifyContent: 'center'
+  },
+  signUpButton: {
+    marginTop: 30,
+    backgroundColor: '#388E3C',
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+    width: '80%',
+    height: 50,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF', // Text color
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+>>>>>>> 0c80e50 (unbreaking...)
 });
 
 export default WelcomePage;
