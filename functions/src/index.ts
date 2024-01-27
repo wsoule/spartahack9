1197bf948d1bc4840fed0125a2a0112ff567c362
KadinMEastway <Kadin.eastway2021@gmail.com>
2024-01-27 17:56:42 -0500

    merge conflict solved -- keep both

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,19 +10,57 @@ import { getFirestore } from "firebase-admin/firestore";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
+<<<<<<< HEAD
 <<<<<<< HEAD:functions/src/index.ts
+=======
+
+// Firebase
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+admin.initializeApp(functions.config().firebase);
+
+// Cloud Vision
+import * as vision from '@google-cloud/vision'
+const visionClient = new vision.ImageAnnotatorClient();
+
+// Bucket name for Cloud function invocation
+const bucketName = 'wastewell-8a42f-vision';
+
+>>>>>>> 80f55f2 (merge conflict solved -- keep both)
 export const imageTagger = functions.firestore.document('photos/{document}')
   .onCreate( async (event: { data: any; }) => {
     // File data
     const object = event.data;
     const filePath = object.name;
+<<<<<<< HEAD
 =======
+=======
+
+    // Location of file saved in bucket
+    const imageUri = `gs://${bucketName}/${filePath}`;
+
+    // Firestore docId === file name
+    const docId =  filePath.split('.jpg')[0];
+
+    const docRef = admin.firestore().collection('photos').doc(docId);
+
+    // Await cloud vision response
+    const results = await visionClient.labelDetection(imageUri);
+
+    // Map results to desired format
+    const labels = results[0].labelAnnotations?.map(obj => obj.description);
+    const metal = labels?.includes('metal');
+
+    return docRef.set({metal, labels});
+  }
+);
+
+>>>>>>> 80f55f2 (merge conflict solved -- keep both)
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
 >>>>>>> 9fb4e7b (add firebase hello world):src/functions/src/index.ts
-
 
 exports.addMessage = onRequest(async (req, res) => {
   const original = req.query.text;
