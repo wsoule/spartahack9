c8509277daafccfbdfe69e8ca584b2fbe6637463
KadinMEastway <Kadin.eastway2021@gmail.com>
2024-01-28 11:32:42 -0500

    home incomplete

--- app/loggedInPages/home.tsx
+++ app/loggedInPages/home.tsx
@@ -7,58 +7,56 @@ import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 const HomePage = () => {
   const username = "John Doe"; // Replace with your own name
   const points = 12;
-  return (
-    <>   
-      <View style={styles.container}>
-        <View style={styles.mainContent}>
-          <TouchableOpacity style={styles.friendsBtn} onPress={() => router.navigate('/loggedInPaged/friends')}>
-            <Text style={styles.points}>Friends</Text>
+  return ( 
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.friendsBtn} onPress={() => router.navigate('/loggedInPaged/friends')}>
+        <Text style={styles.points}>Friends</Text>
+      </TouchableOpacity>
+      <Text style={styles.username}>Username: {username}</Text>
+      <Text style={styles.points}>Points Earned: {points}</Text>
+      <View style={styles.footer}>
+        <Link href="/loggedInPages/cameraModal" asChild>
+          <TouchableOpacity style={styles.iconContainer}>
+            <FontAwesome name="camera" size={35} style={styles.cameraIcon} />
+            {/* <Text style={styles.footerText}>Picture</Text> */}
           </TouchableOpacity>
-          <Text style={styles.username}>Username: {username}</Text>
-          <Text style={styles.points}>Points Earned: {points}</Text>
-        </View>
-        <View style={styles.footer}>
-          <Link href="/loggedInPages/cameraModal" asChild>
-            <TouchableOpacity style={styles.iconContainer}>
-              <FontAwesome name="camera" size={35} style={styles.cameraIcon} />
-              {/* <Text style={styles.footerText}>Picture</Text> */}
-            </TouchableOpacity>
-          </Link>
-          <Link href="/loggedInPages/marketplace" asChild>
-            <TouchableOpacity style={styles.iconContainer}>
-              <FontAwesome
-                name="shopping-bag"
-                size={35}
-                style={styles.shoppingBagIcon}
-              />
-              {/* <Text style={styles.footerText}>Market</Text> */}
-            </TouchableOpacity>
-          </Link>
-          <Link href="/loggedInPages/leaderboard" asChild>
-            <TouchableOpacity style={styles.iconContainer}>
-              <FontAwesome name="trophy" size={35} style={styles.trophyIcon} />
-              {/* <Text style={styles.footerText}>Points</Text> */}
-            </TouchableOpacity>
-          </Link>
-          <Link href="/loggedInPages/settings" asChild>
-            <TouchableOpacity style={styles.iconContainer}>
-              <FontAwesome name="gear" size={35} style={styles.gearIcon} />
-              {/* <Text style={styles.footerText}>Settings</Text> */}
-            </TouchableOpacity>
-          </Link>
-        </View>
+        </Link>
+        <Link href="/loggedInPages/marketplace" asChild>
+          <TouchableOpacity style={styles.iconContainer}>
+            <FontAwesome
+              name="shopping-bag"
+              size={35}
+              style={styles.shoppingBagIcon}
+            />
+            {/* <Text style={styles.footerText}>Market</Text> */}
+          </TouchableOpacity>
+        </Link>
+        <Link href="/loggedInPages/leaderboard" asChild>
+          <TouchableOpacity style={styles.iconContainer}>
+            <FontAwesome name="trophy" size={35} style={styles.trophyIcon} />
+            {/* <Text style={styles.footerText}>Points</Text> */}
+          </TouchableOpacity>
+        </Link>
+        <Link href="/loggedInPages/settings" asChild>
+          <TouchableOpacity style={styles.iconContainer}>
+            <FontAwesome name="gear" size={35} style={styles.gearIcon} />
+            {/* <Text style={styles.footerText}>Settings</Text> */}
+          </TouchableOpacity>
+        </Link>
       </View>
-    </>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   mainContent: {
     // Add styles for your main content area
+    marginTop: 60,
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#00A36C",
+    backgroundColor: "#FFFFFF",
+    color:'FFFFFF'
   },
   container: {
     flex: 1,
@@ -115,20 +113,21 @@ const styles = StyleSheet.create({
   username: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    margin: 0,
+    color: 'white',
   },
   points: {
     fontSize: 18,
-    marginBottom: 20,
+    textAlign: 'center',
+    color: 'white',
+    margin: 0,
   },
   friendsBtn: {
-    marginTop: 30,
     backgroundColor: '#4682b4',
     borderRadius: 10,
-    padding: 10,
     elevation: 2,
-    width: '20%',
-    height: '10%',
+    width: 80,
+    height: 45,
     justifyContent: 'center'
   }
 });
